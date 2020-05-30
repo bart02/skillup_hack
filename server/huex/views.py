b4b25ab21be67326a554413c934487bdafd3c365
Artem Batalov <a@batalov.me>
2020-05-30 21:49:29 +0700

    Fork from HUEX

---
+++ server/huex/views.py
@@ -0,0 +1,301 @@
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from huex.copter import Clever, get_distance, threshold
+import random
+from json import load, dump
+from huex.graphs import build_path, renew
+import logging
+import consts as c
+
+copters = []
+nearest_copter_threshold = c.nearest_copter_threshold
+
+
+logging.disable(logging.CRITICAL)
+
+
+def main(request):
+    data = dict()
+    if get_client_ip(request) in c.allowed_ips:
+        return render(request, "main.html", data)
+    else:
+        return redirect('/m')
+
+
+def guest_page(request):
+    return render(request, 'chpage.html', dict())
+
+
+def mobile(request):
+    data = dict()
+    with open('static/roads.json', 'r') as f:
+        file = load(f)
+        data['array'] = [i for i in range(0, len(file['points']))]
+        data['coords'] = [list(e.values()) for e in file['points']]
+    return render(request, "mobile.html", data)
+
+
+def delete(request):
+    copters.pop(int(request.GET.dict()["id"]))
+    return JsonResponse({})
+
+
+@csrf_exempt
+def post_telemetry(request):
+    ip = get_client_ip(request)
+
+    if not get_client_ip(request) in [i.ip for i in copters]:
+        copters.append(Clever(ip))
+
+    for copter in copters:
+        if copter.ip == ip:
+            copter.x = float(request.GET.get("x"))
+            copter.y = float(request.GET.get("y"))
+            copter.z = float(request.GET.get("z"))
+            # i.yaw = float(request.GET.get("yaw"))
+            if str(float(request.GET.get("cell_voltage"))) == 'nan':
+                copter.voltage = 0
+            else:
+                copter.voltage = float(request.GET.get("cell_voltage"))
+            return JsonResponse(copter.toNewTelem(copters))
+
+
+def get_info(request):
+    data = dict()
+
+    data["message"] = "OK"
+    data["drones"] = []
+
+    for copter in copters:
+        data["drones"].append(copter.toTelem(copters))
+
+    return JsonResponse(data)
+
+
+def random_drone():
+    r = lambda: random.randint(0, 255)
+    return {
+        "led": '#%02X%02X%02X' % (r(), r(), r()),
+        "status": ["landed", "flight"][random.randint(0, 1)],
+        "pose": {
+            "x": random.randint(40, 2500), "y": random.randint(40, 2500), "z": random.randint(40, 2500), "yaw": 3.141592
+        },
+        "next": {
+            "x": random.randint(40, 2500), "y": random.randint(40, 2500), "z": random.randint(40, 2500), "yaw": 3.141592
+        },
+    }
+
+
+def send_command(request):
+    data = request.GET.dict()
+    if data['command'] == 'build_path':
+        try:
+            if not copters[int(data["id"])].path:
+                path = build_path(str(data['o']) + '0', str(data['t']) + '0')
+                copters[int(data["id"])].path += path
+            else:
+                return JsonResponse({'m': 'busy'})
+        except:
+            return JsonResponse({"m": "no way"})
+    elif data['command'] == 'force_land':
+        copters[int(data["id"])].force_landed = True
+    return JsonResponse({"m": "ok"})
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+
+def set_field(request):
+    data = request.GET.dict()
+    with open('static/roads.json', 'r') as f:
+        file_data = load(f)
+
+    if data['m'] == 'add':
+        if data['c'] == 'point':
+            file_data['points'].append({
+                "x": float(data['x']),
+                "y": float(data['y'])
+            })
+        elif data['c'] == 'line':
+            if not {'1': int(data['o']), '2': int(data['t'])} in file_data['lines'] and data['o'] != data['t']:
+                file_data['lines'].append({
+                    '1': int(data['o']),
+                    '2': int(data['t'])
+                })
+    elif data['m'] == 'remove':
+        if data['c'] == 'point':
+            if int(data['n']) != -1:
+                file_data['points'].pop(int(data['n']))
+                i = 0
+                while i < len(file_data["lines"]):
+                    if file_data["lines"][i]["1"] == int(data["n"]) or file_data["lines"][i]["2"] == int(data["n"]):
+                        file_data["lines"].pop(i)
+                    else:
+                        i += 1
+                for i in range(0, len(file_data['lines'])):
+                    if file_data['lines'][i]['1'] > int(data['n']):
+                        file_data['lines'][i]['1'] -= 1
+                    if file_data['lines'][i]['2'] > int(data['n']):
+                        file_data['lines'][i]['2'] -= 1
+        elif data['c'] == 'line':
+            for i in range(0, len(file_data['lines'])):
+                if file_data['lines'][i] == {'1': int(data['o']), '2': int(data['t'])} or file_data['lines'][i] == {
+                    '1': int(data['t']), '2': int(data['o'])}:
+                    file_data['lines'].pop(i)
+                    break
+
+    with open('static/roads.json', 'w') as f:
+        dump(file_data, f)
+    renew()
+    return JsonResponse({})
+
+
+def set_color(request):
+    data = request.GET.dict()
+    copters[int(data['id'])].led = '#' + data['color']
+    return JsonResponse({})
+
+
+def get_dist(request):
+    data = request.GET.dict()
+    path = build_path(str(data['o']) + '0', str(data['t']) + '0')
+    dist = calc_path(path)
+    cost = 150 + dist * 30
+    return JsonResponse({'dist': dist, 'cost': cost})
+
+
+def calc_path(path):
+    dist = 0
+    with open('static/roads.json', 'r') as f:
+        data = load(f)
+    for i in range(0, len(path[:-1])):
+        p1 = {
+            'x': data['points'][int(path[i][:-1])]['x'],
+            'y': data['points'][int(path[i][:-1])]['y'],
+        }
+        if path[i][-1] == 0:
+            p1['z'] = c.first_layer_height
+        else:
+            p1['z'] = c.second_layer_height
+        p2 = {
+            'x': data['points'][int(path[i + 1][:-1])]['x'],
+            'y': data['points'][int(path[i + 1][:-1])]['y'],
+        }
+        if path[i + 1][-1] == 0:
+            p2['z'] = c.first_layer_height
+        else:
+            p2['z'] = c.second_layer_height
+        dist += get_distance(p1['x'], p1['y'], p1['z'], p2['x'], p2['y'], p2['z'])
+    return dist
+
+
+def ask_taxi(request):
+    data = request.GET.dict()
+    busy = 0
+    points = []
+    for i in copters:
+        if i.path != []:
+            busy += 1
+            points.append(-1)
+        else:
+            points.append(get_nearest_point(i))
+
+    if busy == len(copters):
+        return JsonResponse({'m': 'busy'})
+
+    busy_points = []
+
+    for i in copters:
+        busy_points += i.busy_points
+
+    if str(data['o']) + '0' in busy_points or str(data['t']) + '0' in busy_points:
+        return JsonResponse({'m': 'wrong'})
+
+    with open('static/roads.json', 'r') as f:
+        destination_point = load(f)['points'][int(data['t'])]
+
+    for copter in copters:
+        if copter.status == 'land':
+            if get_distance(copter.x, copter.y, 0, destination_point['x'], destination_point['y'],
+                            0) < nearest_copter_threshold:
+                return JsonResponse({'m': 'wrong'})
+
+    paths = []
+    for i in range(0, len(points)):
+        if points[i] != -1:
+            paths.append(calc_path(build_path(str(points[i]) + '0', str(data['o']) + '0')))
+        else:
+            paths.append(9999999)
+
+    nearest_copter = copters[paths.index(min(paths))]
+    first_path = build_path(str(get_nearest_point(nearest_copter)) + '0', str(data['o']) + '0')
+    if len(first_path) != 1:
+        nearest_copter.path += first_path
+        nearest_copter.path.append('-1')
+        nearest_copter.busy_points += [str(data['o']) + '0', str(data['t']) + '0']
+    else:
+        nearest_copter.busy_points += [str(data['t']) + '0']
+    nearest_copter.path += build_path(str(data['o']) + '0', str(data['t']) + '0')
+    r = lambda: random.randint(0, 255)
+    nearest_copter.led = '#%02X%02X%02X' % (r(), r(), r())
+
+    return JsonResponse({'m': 'ok', 'color': nearest_copter.led, 'ip': nearest_copter.ip})
+
+
+def get_nearest_point(c):
+    index = 0
+    min_dist = 9999999
+    with open('static/roads.json', 'r') as f:
+        data = load(f)
+    for i in range(0, len(data['points'])):
+        dist = get_distance(c.x, c.y, 0, data['points'][i]['x'], data['points'][i]['y'], 0)
+        if min_dist > dist:
+            index = i
+            min_dist = dist
+    return index
+
+
+def get_busy_points(request):
+    arr = []
+    with open('static/roads.json', 'r') as f:
+        file_data = load(f)
+    for copter in copters:
+        if copter.status != 'land':
+            try:
+                if copter.last_point != -1:
+                    n = int(copter.path[0][:-1])
+                    nav_point = file_data['points'][n]
+                    nav_point['z'] = c.first_layer_height
+
+                    if copter.path[0][-1:] == '0':
+                        nav_point['z'] = c.first_layer_height
+                    elif copter.path[0][-1:] == '1':
+                        nav_point['z'] = c.second_layer_height
+                    dist = get_distance(nav_point['x'], nav_point['y'], nav_point['z'], copter.x, copter.y, copter.z)
+                    if dist > threshold:
+                        arr.append(int(copter.last_point[:-1]))
+            except:
+                pass
+            try:
+                arr.append(int(copter.path[0][:-1]))
+            except:
+                pass
+
+    return JsonResponse({'busy': arr})
+
+
+def ip_status(request):
+    ip = request.GET.get('ip')
+
+    for copter in copters:
+        if copter.ip == ip:
+            return JsonResponse({"status": copter.get_status()})
+
+    return JsonResponse({"status": 'wrong'})
---
+++ server/static/map.js
@@ -0,0 +1,410 @@
+var canvas = new fabric.Canvas('c');
+canvas.uniScaleKey = "null";
+
+window.addEventListener('resize', resizeCanvas, false);
+
+function resizeCanvas() {
+    var style = window.getComputedStyle(document.getElementById("lf"), null);
+    canvas.setHeight(parseInt(style.getPropertyValue("height")));
+    canvas.setWidth(parseInt(style.getPropertyValue("width")));
+    //imgInstance.scaleToWidth(canvas.width);
+    canvas.renderAll();
+}
+
+
+canvas.setZoom(canvas.getZoom() / 10);
+
+canvas.viewportTransform[4] += canvas.width * 2;
+canvas.viewportTransform[5] += canvas.height * 3;
+
+canvas.renderAll();
+
+var chosen = 0;
+var choses = [];
+
+function loadField() {
+    let request = new XMLHttpRequest();
+    request.open('GET', '/static/map.txt', false);
+    request.send();
+    var markers = [];
+
+    if (request.status === 200) {
+        let arr = request.responseText.split("\n");
+
+        for (let i = 0; i < arr.length; i++) {
+            let str = arr[i].split(" ");
+            let id = parseInt(str[0]);
+            let size = parseFloat(str[1]) * 1000 / 6;
+            let shiftX = parseFloat(str[2]) * 1000;
+            let shiftY = parseFloat(str[3]) * 1000;
+            let angle = parseFloat(str[5]) / Math.PI * 180;
+
+            try {
+                var marker = generateArucoMarker(4, 4, "4x4_1000", id);
+
+                marker.set({
+                    angle: angle,
+                    id: id,
+                    tip: 'm',
+                    scaleX: size,
+                    scaleY: size,
+                    left: shiftX,
+                    top: -shiftY,
+                    marker: true
+                });
+
+                let a = marker.aCoords;
+                let center = {x: (a.bl.x + a.tr.x) / 2, y: (a.bl.y + a.tr.y) / 2};
+
+                marker.set('top', marker.top - (a.tl.y + center.y) * 1000 / 18);
+                marker.set('left', marker.left + (a.tl.x - center.x) * 1000 / 18);
+
+                var text = new fabric.Text(i.toString(), {
+                    fontFamily: 'Delicious_500',
+                    left: marker.left,
+                    top: marker.top,
+                    fill: 'white',
+                    fontSize: 60
+                });
+
+                markers.push(marker);
+                markers.push(text);
+
+            } catch (e) {
+
+            }
+        }
+
+    }
+    let req = new XMLHttpRequest();
+    req.open('POST', '/static/roads.json', false);
+    req.send();
+    if (req.status === 200) {
+        var canvas_data = JSON.parse(req.responseText);
+        roads = canvas_data;
+        for (let i = 0; i < canvas_data.lines.length; i++) {
+            let coords = [canvas_data.points[canvas_data.lines[i]["1"]].x * 1000, -canvas_data.points[canvas_data.lines[i]["1"]].y * 1000, canvas_data.points[canvas_data.lines[i]["2"]].x * 1000, -canvas_data.points[canvas_data.lines[i]["2"]].y * 1000];
+            let line = new fabric.Line(coords, {
+                fill: 'red',
+                stroke: 'red',
+                tip: 'l',
+                strokeWidth: 15,
+            });
+            markers.push(line);
+        }
+        for (let i = 0; i < canvas_data.points.length; i++) {
+            let line = new fabric.Circle({
+                tip: 'c',
+                id: i,
+                radius: 100,
+                fill: 'red',
+                left: canvas_data.points[i].x * 1000 - 100,
+                top: -canvas_data.points[i].y * 1000 - 100,
+                selectable: false
+            });
+            markers.push(line);
+            var text = new fabric.Text(i.toString(), {
+                fontFamily: 'Delicious_500',
+                left: line.left + 75,
+                top: line.top + 50,
+                fill: 'blue',
+                fontSize: 100
+            });
+            markers.push(text);
+        }
+    }
+
+    try {
+        var group = new fabric.Group(markers, {
+            selectable: false
+        });
+        canvas._objects.unshift(group);
+        canvas.renderAll();
+    } catch (e) {
+
+    }
+}
+
+function generateMarkerObject(width, height, bits) {
+    var marker = [];
+
+    // Background rect
+    var backRect = new fabric.Rect({
+        left: 0,
+        top: 0,
+        originX: 'left',
+        originY: 'top',
+        width: width + 2,
+        height: height + 2,
+        fill: 'black'
+    });
+    marker.push(backRect);
+
+    // "Pixels"
+    for (var i = 0; i < height; i++) {
+        for (var j = 0; j < width; j++) {
+            var color = bits[i * height + j] ? 'white' : 'black';
+            var pixel = new fabric.Rect({
+                left: j + 1,
+                top: i + 1,
+                originX: 'left',
+                originY: 'top',
+                width: 1,
+                height: 1,
+                fill: color,
+                hasBorders: false
+            });
+            marker.push(pixel);
+        }
+    }
+    // generate group object
+    var group = new fabric.Group(marker, {
+        width: width + 2,
+        height: height + 2,
+        originX: 'left',
+        originY: 'top',
+        transparentCorners: false,
+        lockScalingFlip: true
+    }).setCoords().setControlsVisibility({
+        'ml': false,
+        'mt': false,
+        'mr': false,
+        'mb': false
+    });
+
+    return group;
+}
+
+function generateArucoMarker(width, height, dictName, id) {
+    var bytes = dict[dictName][id];
+    var bits = [];
+    var bitsCount = width * height;
+
+    // Parse marker's bytes
+    for (var byte of bytes) {
+        var start = bitsCount - bits.length;
+        for (var i = Math.min(7, start - 1); i >= 0; i--) {
+            bits.push((byte >> i) & 1);
+        }
+    }
+
+    return generateMarkerObject(width, height, bits);
+}
+
+
+resizeCanvas();
+loadField();
+canvas.renderAll();
+
+canvas.on('mouse:down', function (opt) {
+    var evt = opt.e;
+    if (evt.altKey === true) {
+        this.isDragging = true;
+        this.selection = false;
+        this.lastPosX = evt.clientX;
+        this.lastPosY = evt.clientY;
+    } else if (choosing === true) {
+        var land_point = opt.absolutePointer;
+        var point1 = get_point(land_point.x, -land_point.y);
+        if (point1 === -1 && choses.length === 0) {
+            /*let request = new XMLHttpRequest();
+            let send_data = {
+                id: choose_id,
+                command: choose_type,
+                x: land_point.x / 1000,
+                y: -land_point.y / 1000,
+                z: 1.5
+            };
+            request.open('GET', '/send?' + Object.entries(send_data).map(e => e.join('=')).join('&'), true);
+            request.send(null);
+            choosing = false;*/
+        } else {
+            if (chosen === 0) {
+                var point1 = get_point(opt.absolutePointer.x, -opt.absolutePointer.y);
+                if (point1 !== -1) {
+                    choses.push(point1);
+                    chosen += 1;
+                    return
+                }
+            } else if (chosen === 1) {
+                var point2 = get_point(opt.absolutePointer.x, -opt.absolutePointer.y);
+                if (point2 !== -1) {
+                    choses.push(point2);
+                    let request = new XMLHttpRequest();
+                    let send_data = {
+                        id: choose_id,
+                        command: 'build_path',
+                        o: choses[0],
+                        t: choses[1]
+                    };
+                    request.open('GET', '/send?' + Object.entries(send_data).map(e => e.join('=')).join('&'), true);
+                    request.send(null);
+                }
+                choses = [];
+                chosen = 0
+            }
+        }
+    } else if (adding_point === true) {
+        let request = new XMLHttpRequest();
+        request.open('GET', '/set?m=add&c=point&x=' + (opt.absolutePointer.x / 1000).toString() + '&y=' + (-opt.absolutePointer.y / 1000).toString(), true);
+        request.send(null);
+        adding_point = false;
+        canvas._objects.shift();
+        loadField();
+    } else if (adding_line === true) {
+        if (chosen === 0) {
+            var point1 = get_point(opt.absolutePointer.x, -opt.absolutePointer.y);
+            if (point1 !== -1) {
+                choses.push(point1);
+                chosen += 1;
+                return
+            }
+        } else if (chosen === 1) {
+            var point2 = get_point(opt.absolutePointer.x, -opt.absolutePointer.y);
+            if (point2 !== -1) {
+                choses.push(point2);
+                let request = new XMLHttpRequest();
+                request.open('GET', '/set?m=add&c=line&o=' + choses[0].toString() + '&t=' + choses[1].toString(), true);
+                request.send(null);
+                adding_line = false;
+                canvas._objects.shift();
+                loadField();
+            }
+            choses = [];
+            chosen = 0
+        }
+    } else if (remove_point === true) {
+        let request = new XMLHttpRequest();
+        request.open('GET', '/set?m=remove&c=point&n=' + get_point(opt.absolutePointer.x, -opt.absolutePointer.y).toString(), true);
+        request.send(null);
+        remove_point = false;
+        canvas._objects.shift();
+        loadField();
+    } else if (remove_line === true) {
+        if (chosen === 0) {
+            var point1 = get_point(opt.absolutePointer.x, -opt.absolutePointer.y);
+            if (point1 !== -1) {
+                choses.push(point1);
+                chosen += 1;
+                return
+            }
+        } else if (chosen === 1) {
+            var point2 = get_point(opt.absolutePointer.x, -opt.absolutePointer.y);
+            if (point2 !== -1) {
+                choses.push(point2);
+                let request = new XMLHttpRequest();
+                request.open('GET', '/set?m=remove&c=line&o=' + choses[0].toString() + '&t=' + choses[1].toString(), true);
+                request.send(null);
+                remove_line = false;
+                canvas._objects.shift();
+                loadField();
+            }
+            choses = [];
+            chosen = 0
+        }
+    }
+    canvas.backgroundColor = "#f9f9f9";
+    for (let i = 0; i < canvas._objects.length; i++) {
+        canvas._objects[i].set('opacity', 1);
+    }
+    canvas.renderAll();
+});
+
+canvas.on('mouse:move', function (opt) {
+    if (this.isDragging) {
+        var e = opt.e;
+        this.viewportTransform[4] += e.clientX - this.lastPosX;
+        this.viewportTransform[5] += e.clientY - this.lastPosY;
+        this.requestRenderAll();
+        this.lastPosX = e.clientX;
+        this.lastPosY = e.clientY;
+    }
+});
+
+canvas.on('mouse:up', function (opt) {
+    this.isDragging = false;
+    this.selection = true;
+});
+
+canvas.on('mouse:wheel', function (opt) {
+    var delta = opt.e.deltaY;
+    var zoom = canvas.getZoom();
+    zoom = zoom + delta / 200;
+    if (zoom > 20) zoom = 20;
+    if (zoom < 0.01) zoom = 0.01;
+    canvas.zoomToPoint({x: opt.e.offsetX, y: opt.e.offsetY}, zoom);
+    opt.e.preventDefault();
+    opt.e.stopPropagation();
+    canvas.renderAll();
+});
+
+
+function drawDrones() {
+    var thr = 1;
+    if (choosing) {
+        thr = 1;
+    }
+
+    while (canvas._objects.length - thr < curr_telemetry.length) {
+        let circ = new fabric.Circle({
+            radius: 100, fill: 'green', left: 100, top: 100, selectable: false
+        });
+        canvas.add(circ);
+    }
+    while (canvas._objects.length - thr > curr_telemetry.length) {
+        canvas._objects.pop();
+    }
+
+    let req = new XMLHttpRequest();
+    req.open('GET', '/get_busy_points', false);
+    req.send(null);
+    if (req.status === 200 && canvas._objects[0]._objects !== null) {
+        bpoints = JSON.parse(req.responseText).busy;
+        if (bpoints.length !== 0) {
+            for (let j = 0; j < canvas._objects[0]._objects.length; j++) {
+                if (canvas._objects[0]._objects[j].tip === 'c') {
+                    canvas._objects[0]._objects[j].fill = 'red';
+                }
+            }
+        }
+        for (let i = 0; i < bpoints.length; i++) {
+            for (let j = 0; j < canvas._objects[0]._objects.length; j++) {
+                if (canvas._objects[0]._objects[j].tip === 'c') {
+                    if (canvas._objects[0]._objects[j].id === bpoints[i]) {
+                        canvas._objects[0]._objects[j].set('fill', '#FFFF00');
+                    }
+                }
+            }
+        }
+    }
+    canvas.renderAll();
+
+    for (let i = 0; i < curr_telemetry.length; i++) {
+        canvas._objects[i + 1].set('fill', curr_telemetry[i].led);
+        canvas._objects[i + 1].animate('left', curr_telemetry[i].pose.x * 1000 - 100, {
+            duration: 1000 / freq,
+            easing: fabric.util.ease.easeInSine
+        });
+        canvas._objects[i + 1].animate('top', -curr_telemetry[i].pose.y * 1000 - 100, {
+            onChange: canvas.renderAll.bind(canvas),
+            duration: 1000 / freq,
+            easing: fabric.util.ease.easeInSine
+        });
+        /*canvas._objects[i + 1].left = curr_telemetry[i].pose.x;
+        canvas._objects[i + 1].top = -curr_telemetry[i].pose.y - 100;*/
+    }
+    canvas.renderAll();
+}
+
+function get_point(x, y) {
+    for (var i = 0; i < roads.points.length; i++) {
+        //console.log([x, roads.points[i].x, y, roads.points[i].y]);
+        let lena = Math.sqrt(Math.pow(x - roads.points[i].x * 1000, 2) + Math.pow(y - roads.points[i].y * 1000, 2));
+        //console.log(lena);
+        if (lena <= 100) {
+            return i
+        }
+    }
+
+    return -1
+}
